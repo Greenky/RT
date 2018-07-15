64ca900be0ceea0758fa2e63fb9b01451df54217
Illia KACHKO <ikachko@e1r11p1.unit.ua>
2018-07-15 11:04:54 +0300

    mac

--- source/main.c
+++ source/main.c
@@ -12,8 +12,10 @@
 
 #include "../includes/rt_functions.h"
 
-int			WinMain(int argc, char **argv)
+int			main(int argc, char **argv)
 {
+	argc = 1;
+	argv = NULL;
 	t_rt	rt_data;
 //	if (argc != 2)
 //		error_exit(NUM_OF_ARG, NULL);

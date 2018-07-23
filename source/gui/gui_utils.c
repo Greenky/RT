17b3257b2ae6d51f1382ce32bc52cc57009d42f0
Illia KACHKO <ikachko@e1r2p13.unit.ua>
2018-07-23 21:31:04 +0300

    gui && parcing_function && find_intersection norming finished

--- source/gui/gui_utils.c
+++ source/gui/gui_utils.c
@@ -12,7 +12,7 @@
 
 #include "../includes/rt_functions.h"
 
-char*	ftoa(float f, int tochnost)
+char	*ftoa(float f, int tochnost)
 {
 	char	*first_part;
 	char	*second_part;
@@ -39,18 +39,51 @@ char*	ftoa(float f, int tochnost)
 	return (first_part);
 }
 
-void		blit_surface_data(t_rt *rt_data, float fdata, SDL_Rect *pos)
+void	blit_surface_data(t_rt *rt_data, float fdata, SDL_Rect *pos)
 {
-	char		*str_data;
+	char	*str_data;
 
 	str_data = ftoa(fdata, 1);
-	SDL_BlitSurface(TTF_RenderText_Shaded(rt_data->gui.open_sans, str_data, BLACK, WHITE), NULL, rt_data->screen_surface, pos);
+	SDL_BlitSurface(TTF_RenderText_Shaded(rt_data->gui.open_sans,
+		str_data, BLACK, WHITE), NULL, rt_data->screen_surface, pos);
 	free(str_data);
 }
 
-void		sdl_error(char *str)
+void	sdl_error(char *str)
 {
 	ft_putstr(str);
 	ft_putendl(SDL_GetError());
 	exit(1);
+}
+
+void	make_screenshot(t_rt *rt_data)
+{
+	SDL_Surface	*surface;
+	static int	img_num;
+	char		*num;
+	char		*scr_name;
+
+	rt_data->take_screenshot = 1;
+	draw_scene(rt_data);
+	surface = SDL_GetWindowSurface(rt_data->window);
+	surface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGB24, 0);
+	num = ft_itoa(img_num);
+	scr_name = ft_strjoin("ScreenShot", num);
+	free(num);
+	num = scr_name;
+	scr_name = ft_strjoin(scr_name, ".jpg");
+	free(num);
+	SDL_SaveBMP(surface, scr_name);
+	rt_data->take_screenshot = 0;
+	img_num++;
+}
+
+void	swap_cartoon(t_intersect closest_inter, t_rt *rt_data, int i)
+{
+	if (closest_inter.fig->type != PLANE
+		&& closest_inter.fig == (rt_data->objects_arr + i))
+		rt_data->objects_arr[i].is_cartoon =
+				rt_data->objects_arr[i].is_cartoon ? 0 : 1;
+	else
+		rt_data->objects_arr[i].is_cartoon = 0;
 }

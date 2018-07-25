f91edb084bdca2b24a59e5196fc51aafde96c303
Валентин Мазурок <valentin.mazurok@gmail.com>
2018-07-25 22:42:39 +0300

    nache roboche, ale hz

--- source/parcing_function/list_to_objects.c
+++ source/parcing_function/list_to_objects.c
@@ -29,6 +29,15 @@ void			fill_object(t_rt *rt_data, t_objects *step_obj, int len)
 	rt_data->objects_arr[len].texture_index = step_obj->texture_index;
 	rt_data->objects_arr[len].is_cartoon = 0;
 	rt_data->objects_arr[len].next = NULL;
+	rt_data->objects_arr[len].cap[0].dist = INFINITY;
+	rt_data->objects_arr[len].cap[1].dist = INFINITY;
+	if (step_obj->type == CYLINDER)
+	{
+		rt_data->objects_arr[len].cap[0].normal = normalize_vector(VEC(0, 1, 1));
+		rt_data->objects_arr[len].cap[1].normal = VEC(0, -1, 0);
+		rt_data->objects_arr[len].cap[0].dist = 1;
+		rt_data->objects_arr[len].cap[1].dist = -1;
+	}
 }
 
 void			fill_light(t_rt *rt_data, t_light *step_light, int len)
